8e2b6228de7660a02019fc900ea0b9b4cc977ba8
ahmed-essam-shaheen <ahmadessamshaheen@gmail.com>
2021-06-13 15:19:52 +0200

    [WIP]NSB-1: add package alias and webpack

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+/* eslint-disable import/no-self-import */
+/* eslint-disable import/no-unresolved */
+const nodeExternals = require('webpack-node-externals');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './src/index.ts',
+  mode: process.env.NODE_ENV ? process.env.NODE_ENV : 'development',
+  devtool:
+    process.env.NODE_ENV === 'production' ? 'source-map' : 'eval-source-map',
+  resolve: {
+    root: path.resolve(__dirname),
+    modules: ['node_modules'],
+    extensions: ['.mjs', '.json', '.ts', '.js', ''],
+    symlinks: false,
+    cacheWithContext: false,
+    alias: {
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@src': path.resolve(__dirname, './src')
+    }
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js'
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  optimization: {
+    minimize: false
+  },
+  performance: {
+    hints: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        include: [path.resolve(__dirname, 'src')],
+        exclude: [
+          [
+            path.resolve(__dirname, 'node_modules'),
+            path.resolve(__dirname, '.webpack')
+          ]
+        ],
+        options: {
+          transpileOnly: true,
+          experimentalWatchApi: true
+        }
+      }
+    ]
+  },
+  plugins: [new ForkTsCheckerWebpackPlugin()]
+};
