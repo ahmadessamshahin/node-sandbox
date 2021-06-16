ef9d9167ed7d1b58f33c2dcde5adec1dee31cc13
ahmed-essam-shaheen <ahmadessamshaheen@gmail.com>
2021-06-16 10:50:16 +0200

    [WIP]NSB-2: split api from the server configration

--- src/api/initializers/index.ts
+++ src/api/initializers/index.ts
@@ -1,12 +1,13 @@
-import "reflect-metadata";
-import express from 'express';
 import { InversifyExpressServer } from "inversify-express-utils";
-import morganMiddleware from "@config/middleware/morganRest";
 import container from "./inversify.config";
 import "@api/controllers/index"
+import app from './routes'
 
-const app = express();
-app.use(morganMiddleware)
-let server = new InversifyExpressServer(container, null, { rootPath: "/api" }, app);
+
+/**
+ *  Server configuration
+ */
+
+const server = new InversifyExpressServer(container, null, { rootPath: "/api" }, app);
 
 export default server.build();
---
+++ src/api/initializers/routes.ts
@@ -0,0 +1,14 @@
+import "reflect-metadata";
+import express from 'express';
+import morganMiddleware from "@config/middleware/morganRest";
+
+const app = express();
+
+
+/**
+ * MiddleWare
+ */
+
+app.use(morganMiddleware)
+
+export default app
